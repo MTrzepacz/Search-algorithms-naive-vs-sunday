b16a82cd331b575e6e06fbad188646d6a1c34774
mateusz trzepacz <mateusz.trzepacz@gmail.com>
2020-12-01 16:47:27 +0100

    polished some graph creation, applied some better practices

--- projekt1.py
+++ projekt1.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from random import choice
-from matplotlib.collections import EventCollection
+from string import ascii_uppercase
 
 #This can be used to generate both pattern and 'searchString'
 def randomString(dl: int, alfabet: str) -> str:
@@ -65,24 +64,29 @@ def createResultGrapth(xAxisParameters, yAxisParametersNaive,yAxisParametersSund
 
 
 if __name__ == "__main__":
-    dic = prepareLastP("ABCD", "ABA")
-    print(naiveV2("ACBCDABABBDB", "ABA"))
-    r1 = naiveV2("ACBCDABABBDB", "ABA")
-    r2 = naiveV2("ACBCDABABBDBACBCDABABBDB", "ABA")
-    r3 = naiveV2("ACBCDABABBDBACBCDABABBDBACBCDABABBDB", "ABA")
-    print(sundayV2("ACBCDABABBDB","ABA",dic))
-    s1 = sundayV2("ACBCDABABBDB","ABA",dic)
-    s2 = sundayV2("ACBCDABABBDBACBCDABABBDB","ABA",dic)
-    s3 = sundayV2("ACBCDABABBDBACBCDABABBDBACBCDABABBDB","ABA",dic)
+    #Creating alphabet, pattern to search and searchString length table
+    searchStringLength = [50, 150, 500]
+    alphabet = ascii_uppercase[:3]
+    pattern = randomString(3, ascii_uppercase[:3])
     
-   
-    createResultGrapth([12,24,36], [r1, r2, r3],[s1, s2, s3], 3 )
-   # fig, ax = plt.subplots()  # Create a figure containing a single axes.
-   # ax.plot([12,24,36],[r1, r2, r3], label = "naive" )
-   # ax.plot([12,24,36],[s1, s2, s3], label = "sunday" )
-   # ax.set_xlabel('Length of searchString')
-   # ax.set_ylabel('Compare Count')
-   # ax.set_title("Search algorithm - Naive vs Sunday with patten length = 3")
-   # ax.legend()
-   # fig.show()
+    print(pattern)
+    
+    #Running Naive algorithm
+    r1 = naiveV2(randomString(searchStringLength[0],  alphabet), pattern)
+    r2 = naiveV2(randomString(searchStringLength[1], alphabet), pattern)
+    r3 = naiveV2(randomString(searchStringLength[2], alphabet), pattern)
+  
+    #Running Sunday algorithm with 
+    dic = prepareLastP(alphabet, pattern)
+    s1 = sundayV2(randomString(searchStringLength[0], alphabet),pattern,dic)
+    s2 = sundayV2(randomString(searchStringLength[1], alphabet),pattern,dic)
+    s3 = sundayV2(randomString(searchStringLength[2], alphabet),pattern,dic)
+    
+    print("Naive counts:")
+    print(*[str(r1),str(r2),str(r3)], sep = ' ')
+    
+    print("Sunday counts:")
+    print(*[str(s1),str(s2),str(s3)], sep = ' ')
+    #Creating result Graph
+    createResultGrapth([50,150,500], [r1, r2, r3],[s1, s2, s3], len(alphabet) )
     
