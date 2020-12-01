262e92b0283b53bd4b99c1524809e83a6dced9bb
mateusz trzepacz <mateusz.trzepacz@gmail.com>
2020-12-01 15:53:02 +0100

    moved graph creation to separate function

--- projekt1.py
+++ projekt1.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from random import choice
+from matplotlib.collections import EventCollection
 
 #This can be used to generate both pattern and 'searchString'
 def randomString(dl: int, alfabet: str) -> str:
@@ -16,13 +19,12 @@ def matchesAt(searchString, pattern, position):
     else:    
         return [True, compareCount]
 
-def prepareLastP(alfabet):
+def prepareLastP(alfabet, pattern):
     dic = {i : -1 for i in alfabet}
-    #print(dic)
-    for i in "ABA":
+    for i in pattern:
         for key, value in dic.items():
             if (key == i):
-                dic[i] = "ABA".rindex(i)
+                dic[i] = pattern.rindex(i)
     return dic            
 
 
@@ -51,7 +53,36 @@ def sundayV2(searchString, pattern, lastp):
     return compareCount        
  
 
+def createResultGrapth(xAxisParameters, yAxisParametersNaive,yAxisParametersSunday,patternLength):
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    ax.plot(xAxisParameters,yAxisParametersNaive, label = "naive" )
+    ax.plot(xAxisParameters,yAxisParametersSunday, label = "sunday" )
+    ax.set_xlabel('Length of searchString')
+    ax.set_ylabel('Compare Count')
+    ax.set_title("Search algorithm - Naive vs Sunday with patten length = " + str(patternLength))
+    ax.legend()
+    fig.show()    
+
+
 if __name__ == "__main__":
-    dic = prepareLastP("ABCD")
+    dic = prepareLastP("ABCD", "ABA")
     print(naiveV2("ACBCDABABBDB", "ABA"))
+    r1 = naiveV2("ACBCDABABBDB", "ABA")
+    r2 = naiveV2("ACBCDABABBDBACBCDABABBDB", "ABA")
+    r3 = naiveV2("ACBCDABABBDBACBCDABABBDBACBCDABABBDB", "ABA")
     print(sundayV2("ACBCDABABBDB","ABA",dic))
+    s1 = sundayV2("ACBCDABABBDB","ABA",dic)
+    s2 = sundayV2("ACBCDABABBDBACBCDABABBDB","ABA",dic)
+    s3 = sundayV2("ACBCDABABBDBACBCDABABBDBACBCDABABBDB","ABA",dic)
+    
+   
+    createResultGrapth([12,24,36], [r1, r2, r3],[s1, s2, s3], 3 )
+   # fig, ax = plt.subplots()  # Create a figure containing a single axes.
+   # ax.plot([12,24,36],[r1, r2, r3], label = "naive" )
+   # ax.plot([12,24,36],[s1, s2, s3], label = "sunday" )
+   # ax.set_xlabel('Length of searchString')
+   # ax.set_ylabel('Compare Count')
+   # ax.set_title("Search algorithm - Naive vs Sunday with patten length = 3")
+   # ax.legend()
+   # fig.show()
+    
